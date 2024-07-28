0e86fd8d807875e20e70ddb11738ff5cf23cff16
shravan <shravankumar6262@gmail.com>
2024-07-28 23:19:07 +0530

    committed by shravan

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
-domain_name="daws78s.online"
-hosted_zone_id="Z08884492QFPW45HM4UQO"
+domain_name="shravan.cloud"
+hosted_zone_id="Z0300022NU5KWUM4EAW3"
+ami_id="ami-041e2ea9402c46c32"
+sg_id="sg-018a7f01c6abc8b7d"
+subnet_id="subnet-0a227ff935177fc05"
 
 for name in ${instances[@]}; do
     if [ $name == "shipping" ] || [ $name == "mysql" ]
@@ -12,7 +15,7 @@ for name in ${instances[@]}; do
         instance_type="t3.micro"
     fi
     echo "Creating instance for: $name with instance type: $instance_type"
-    instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type --security-group-ids sg-0fea5e49e962e81c9 --subnet-id subnet-09863c54177764565 --query 'Instances[0].InstanceId' --output text)
+    instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type $instance_type --security-group-ids $sg_id --subnet-id $subnet_id --query 'Instances[0].InstanceId' --output text)
     echo "Instance created for: $name"
 
     aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
